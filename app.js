e930c7c6cb63425fa64a85ff9491731d981f4115
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-12-10 14:16:06 -0400

    products section added

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import data from "./data.js"
 
-console.log(data[1]);
-console.log("hello wolrd");
+const productGrid = document.querySelector("#product-grid");
+console.log(productGrid)
+data.forEach(item => {
+    const article = document.createElement('article');
+    article.classList.add("product")
+    article.innerHTML = `<img src="${item.img}" height="1000" width="1000">
+                        <h4>${item.title}</h4>
+                        <p>${item.price}</p>
+                        <p>${item.description}</p>`
+    productGrid.appendChild(article)
+})
--- data.js
+++ data.js
@@ -4,56 +4,64 @@ const data = [
         title: "Black Shirt",
         price: "18.99$",
         category: "shirt",
-        img: "images/black-shirt.jpg"
+        img: "images/black-shirt.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 1,
-        title: "Lion Shirt",
-        price: "25.55$",
+        title: "Red Shirt",
+        price: "25$",
         category: "shirt",
-        img: "images/lion-shirt.jpg"
+        img: "images/red-shirt.jpg",
+        description: "Red Shirt ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 2,
-        title: "Green Pants",
+        title: "Black Pants",
         price: "28.99$",
         category: "pants",
-        img: "images/green-pants.jpg"
+        img: "images/black-pants.jpg",
+        description: "Black Pants dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 3,
         title: "Blue Jeans",
         price: "38.00$",
         category: "pants",
-        img: "images/blue-jeans.jpg"
+        img: "images/blue-jeans.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 4,
-        title: "Maroon Dress",
+        title: "Pink Dress",
         price: "78.50$",
         category: "dress",
-        img: "images/maroon-dress.jpg"
+        img: "images/pink-dress.jpg",
+        description: "Pink Dress dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 5,
         title: "Red Dress",
         price: "68.99$",
         category: "dress",
-        img: "images/red-dress.jpg"
+        img: "images/red-dress.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 6,
         title: "Navy Jacket",
         price: "45.99$",
         category: "jacket",
-        img: "images/navy-jacket.jpg"
+        img: "images/navy-jacket.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     },
     {
         id: 7,
         title: "Yellow Jacket",
         price: "79.99$",
         category: "jacket",
-        img: "images/yellow-jacket.jpg"
+        img: "images/yellow-jacket.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. In tenetur reiciendis pariatur adipisci repellat vel, vitae sunt blanditiis voluptatum quae!"
     }
 ]
 
