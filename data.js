cd51b2a4dedab862b7a093f3fd9f40670b434d7d
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-12-10 13:33:28 -0400

    added the data and the images

---
+++ data.js
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        id: 0,
+        title: "Black Shirt",
+        price: "18.99$",
+        category: "shirt",
+        img: "images/black-shirt.jpg"
+    },
+    {
+        id: 1,
+        title: "Lion Shirt",
+        price: "25.55$",
+        category: "shirt",
+        img: "images/lion-shirt.jpg"
+    },
+    {
+        id: 2,
+        title: "Green Pants",
+        price: "28.99$",
+        category: "pants",
+        img: "images/green-pants.jpg"
+    },
+    {
+        id: 3,
+        title: "Blue Jeans",
+        price: "38.00$",
+        category: "pants",
+        img: "images/blue-jeans.jpg"
+    },
+    {
+        id: 4,
+        title: "Maroon Dress",
+        price: "78.50$",
+        category: "dress",
+        img: "images/maroon-dress.jpg"
+    },
+    {
+        id: 5,
+        title: "Red Dress",
+        price: "68.99$",
+        category: "dress",
+        img: "images/red-dress.jpg"
+    },
+    {
+        id: 6,
+        title: "Navy Jacket",
+        price: "45.99$",
+        category: "jacket",
+        img: "images/navy-jacket.jpg"
+    },
+    {
+        id: 7,
+        title: "Yellow Jacket",
+        price: "79.99$",
+        category: "jacket",
+        img: "images/yellow-jacket.jpg"
+    }
+]
